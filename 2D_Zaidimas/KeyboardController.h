10cce57cc38815e5cc694ba11311ebc33f7d9991
Deividas Balinskis <deividasbalinskis@gmail.com>
2020-10-23 22:35:13 +0300

    Animacij. Skirtingu animaciju visuma, pvz juda ir stovi, prdejau animacijos keitima vaiksciojant

--- 2D_Zaidimas/KeyboardController.h
+++ 2D_Zaidimas/KeyboardController.h
@@ -7,10 +7,12 @@ class KeyboardController : public Component
 {
 public:
 	TransformComponent* transform;
+	SpriteComponent* sprite;
 
 	void init() override
 	{
 		transform = &entity->getComponent<TransformComponent>();
+		sprite = &entity->getComponent<SpriteComponent>();
 	}
 
 	void update() override
@@ -21,15 +23,20 @@ public:
 			{
 			case SDLK_w:
 				transform->velocity.y = -1;
+				sprite->Play("walk");
 				break;
 			case SDLK_a:
 				transform->velocity.x = -1;
+				sprite->Play("walk");
+				sprite->spriteFlip = SDL_FLIP_HORIZONTAL;
 				break;
 			case SDLK_d:
 				transform->velocity.x = 1;
+				sprite->Play("walk");
 				break;
 			case SDLK_s:
 				transform->velocity.y = 1;
+				sprite->Play("walk");
 				break;
 			default:
 				break;
@@ -42,15 +49,20 @@ public:
 			{
 			case SDLK_w:
 				transform->velocity.y = 0;
+				sprite->Play("idle");
 				break;
 			case SDLK_a:
 				transform->velocity.x = 0;
+				sprite->Play("idle");
+				sprite->spriteFlip = SDL_FLIP_NONE;
 				break;
 			case SDLK_d:
 				transform->velocity.x = 0;
+				sprite->Play("idle");
 				break;
 			case SDLK_s:
 				transform->velocity.y = 0;
+				sprite->Play("idle");
 				break;
 			default:
 				break;
--- 2D_Zaidimas/SpriteComponent.h
+++ 2D_Zaidimas/SpriteComponent.h
@@ -2,6 +2,8 @@
 #include "Components.h"
 #include "SDL.h"
 #include "TextureManager.h"
+#include "Animation.h"
+#include <map>
 
 class SpriteComponent : public Component
 {
@@ -10,12 +12,38 @@ private:
 	SDL_Texture* texture;
 	SDL_Rect srcRect, destRect;
 
+	bool animated = false;
+	int frames = 0;
+	int speed = 100;
+
 public:
+
+	int animIndex = 0;
+	std::map<const char*, Animation> animations;
+
+	SDL_RendererFlip spriteFlip = SDL_FLIP_NONE;
+
 	SpriteComponent() = default;
 	SpriteComponent(const char* path)
 	{
 		setTex(path);
 	}
+
+	SpriteComponent(const char* path, bool isAnimated)
+	{
+		animated = isAnimated;
+
+		Animation idle = Animation(0, 3, 100);
+		Animation walk = Animation(1, 8, 100);
+
+		animations.emplace("idle", idle);
+		animations.emplace("walk", walk);
+
+		Play("idle");
+
+		setTex(path);
+	}
+
 	~SpriteComponent()
 	{
 		SDL_DestroyTexture(texture);
@@ -36,6 +64,13 @@ public:
 	}
 	void update() override
 	{
+		if (animated)
+		{
+			srcRect.x = srcRect.w * static_cast<int>((SDL_GetTicks() / speed) % frames);
+		}
+
+		srcRect.y = animIndex * transform->height;
+
 		destRect.x = static_cast<int>(transform->position.x);
 		destRect.y = static_cast<int>(transform->position.y);
 		destRect.w = transform->width * transform->scale;
@@ -43,6 +78,13 @@ public:
 	}
 	void draw() override
 	{
-		TextureManager::Draw(texture, srcRect, destRect);
+		TextureManager::Draw(texture, srcRect, destRect, spriteFlip);
+	}
+
+	void Play(const char* animName)
+	{
+		frames = animations[animName].frames;
+		animIndex = animations[animName].index;
+		speed = animations[animName].speed;
 	}
 };
